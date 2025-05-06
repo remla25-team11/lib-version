c658559e91b2bbd1fb1f7b21b379286fa21c30af
KiwiSel <93141025+KiwiSel@users.noreply.github.com>
2025-05-06 03:02:17 +0200

    Update version_util.py

--- version_util.py
+++ version_util.py
@@ -1,1 +1,11 @@
+import importlib.metadata
 
+class VersionUtil:
+    def __init__(self, package_name="lib-version"):
+        self.package_name = package_name
+
+    def get_version(self):
+        try:
+            return importlib.metadata.version(self.package_name)
+        except importlib.metadata.PackageNotFoundError:
+            return "unknown"
